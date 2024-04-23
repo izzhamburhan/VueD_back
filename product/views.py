121c016a56840004d0099bc91333994c96067dc7
izzhamburhan <maizzham@gmail.com>
2024-04-23 14:52:03 +0800

    first add file backend

---
+++ product/views.py
@@ -0,0 +1,26 @@
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Category, Product
+from .serializers import ProductSerializer
+
+class LatestProductList(APIView):
+    def get(self, request, format=None):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+class ProductDetail(APIView):
+    def get_object(self, category_slug, product_slug):
+        try:
+            return Product.objects.filter(category__slug=category_slug).get(slug=product_slug)
+        except Product.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, category_slug, product_slug, format=None):
+        product = self.get_object(category_slug, product_slug)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+
